b0816a6c3c03558390a591285b3897595a5c3754
Andrew Ferguson <ferguson.am@gmail.coml>
2024-01-17 18:45:35 -0500

    initial commit

---
+++ scripts.js
@@ -0,0 +1,72 @@
+const turnRightButton = document.querySelector("#turn");
+const goForwardButton = document.querySelector("#forward");
+const roombaElement = document.querySelector(".roomba");
+
+const initDirection = "up";
+const directions = ["up", "right", "down", "left"];
+let currentX = 0;
+let currentY = 0;
+let currentDirection = 0;
+
+function turnRight() {
+  const directionCount = directions.length;
+  //remove current direction class
+  roombaElement.classList.remove(directions[currentDirection]);
+
+  //point to new direction
+  currentDirection++;
+
+  if (currentDirection > directionCount - 1) {
+    currentDirection = 0;
+    roombaElement.classList.add(initDirection);
+  }
+
+  roombaElement.classList.add(directions[currentDirection]);
+}
+
+turnRightButton.addEventListener("click", turnRight);
+
+goForwardButton.addEventListener("click", () => {
+  const directionClass = directions[currentDirection];
+
+  if (directionClass === "right") {
+    currentX += 1;
+  } else if (directionClass === "left") {
+    currentX -= 1;
+  }
+
+  if (directionClass === "up") {
+    currentY -= 1;
+  } else if (directionClass === "down") {
+    currentY += 1;
+  }
+
+  if (directionClass === "right" && currentX > 9) {
+    currentX = 9;
+    turnRight();
+  }
+
+  if (directionClass === "down" && currentY > 9) {
+    currentX = 9;
+    turnRight();
+  }
+
+  if (directionClass === "left" && currentX < 0) {
+    currentX = 0;
+    turnRight();
+  }
+
+  if (directionClass === "up" && currentY < 0) {
+    currentY = 0;
+    turnRight();
+  }
+
+  const moveToColumn = document.querySelector(
+    `.Grid > .Column:nth-child(${currentX + 1})`
+  );
+  const moveToCell = moveToColumn.querySelector(
+    `.Cell:nth-child(${currentY + 1})`
+  );
+
+  moveToCell.appendChild(roombaElement);
+});
